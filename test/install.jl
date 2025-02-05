1b88f486a5bb4e095b3643e3e10a7fbc6140825e
Max <maximilian.bittens@bgr.de>
2025-02-05 11:28:40 +0100

    Create install.jl

---
+++ test/install.jl
@@ -0,0 +1,8 @@
+import Pkg
+Pkg.add("DistributedSparseGrids")
+Pkg.add("Ogs6InputFileHandler")
+Pkg.add("OpenGeoSysUncertaintyQuantification")
+Pkg.add("GLMakie")
+Pkg.add("Printf")
+Pkg.add("CoordinateTransformations")
+Pkg.add("Distributions")
